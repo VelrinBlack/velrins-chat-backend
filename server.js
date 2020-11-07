f9aa69cd11d7598dc266b6b991a2fd077a10d42f
velrinblack <velrinblack@gmail.com>
2020-11-07 16:22:36 +0100

    Add CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import connectDB from './db.js';
 import userRoute from './routes/api/user.js';
@@ -7,6 +8,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/user', userRoute);
 
